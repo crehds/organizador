66bf82e0c95fbae844e7e0b70d237c98ba8054af
Carlos Rodriguez <carlos15erh@gmail.com>
2023-01-13 06:35:26 -0500

    Fix: register controller in esbuild

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,4 +9,8 @@
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+import ResetForm from './reset_form_controller';
+import Modal from './modal_controller';
 import { application } from "./application";
+application.register("reset-form", ResetForm)
+application.register("modal", Modal)
